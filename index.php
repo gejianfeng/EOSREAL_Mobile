15e1697caddb31a72bf691f515750e4818c894df
Ge Jian Feng <gejianfeng2@126.com>
2018-04-26 02:16:12 +0800

    + add

--- index.php
+++ index.php
@@ -11,7 +11,14 @@ Template Name: index.php
 	<div class="logo"></div>
 	<div class="sub-title">The most powerful infrastructure for <br/>decentralized applications</div>
 	<div class="arrow"></div>
-	<div class="icon email"></div>
+	<div class="icon email" onclick="window.location.href='mailto:eos@eosreal.com';"></div>
+	<div class="icon twitter" onclick="window.location.href='https://twitter.com/EOSREAL_IO';"></div>
+	<div class="icon wechat" onclick=""></div>
+	<div class="icon weibo" onclick="window.location.href='https://www.weibo.com/u/6527955391?topnav=1&wvr=6&topsug=1';"></div>
+	<div class="icon telegram" onclick="window.location.href='https://t.me/joinchat/G6-t7RKGUdGWu-Q8MA5IKg';"></div>
+	<div class="icon medium" onclick="window.location.href='https://medium.com/@EOSREAL_IO';"></div>
+	<div class="icon treemit" onclick="window.location.href='https://steemit.com/@eosreal-io';"></div>
+	<div class="icon youtobe" onclick="window.location.href='https://www.youtube.com/channel/UCDa2gIsEQway1DPNjMYar8g?view_as=subscriber';"></div>
 </div>
 
 <?php get_footer(); ?>
